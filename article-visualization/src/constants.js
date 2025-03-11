2c5cd3461a5e26d846bb842898f1794b2fa221a9
singvir23 <viraajsingh135@gmail.com>
2025-03-11 17:54:36 -0400

    switched colors

--- article-visualization/src/constants.js
+++ article-visualization/src/constants.js
@@ -1,7 +1,7 @@
 // src/constants.js
 export const COLORS = {
-    CNN: '#f87171', // Soft Red
-    FOX: '#60a5fa', // Soft Blue
+    CNN: '#60a5fa', // Soft Red
+    FOX: '#f87171', // Soft Blue
     // Add more sources if needed
   };
   
